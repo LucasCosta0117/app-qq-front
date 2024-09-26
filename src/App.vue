9bb0b3088626e9d76a5fbb10aadf309d8bac8614
Lucas Costa <lucas.costa@simova.com.br>
2024-09-26 16:31:01 -0300

    Upload project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>My Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer app>
+      <v-list>
+        <v-list-item @click="$router.push('/')">
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="$router.push('/about')">
+          <v-list-item-title>About</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
